ca2ae8437037a9fa956fbbcb6bb8c0edb7152be8
Frantz Augustin <hirefrantz@gmail.com>
2016-09-16 22:19:44 -0400

    2.0

---
+++ js/message.js
@@ -0,0 +1,5 @@
+$('body').append("<div class='notice'><p>This portfolio is under contruction.</p>
+ <p>All the content will be available soon!</p></div>");
+setTimeout(function() {
+  $('.message').remove();
+}, 5000)
