16576bf9791b4c1358d561c31e81328299e1cd1f
Frantz Augustin <hirefrantz@gmail.com>
2016-09-17 21:55:23 -0400

    2.0

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
   var $menu = $('.menu');
 
   $menu.on('click', function(){
+
+    $(".menu-open").css({"background-color": "white"});
     $(this).toggleClass('menu-open');
+
   });
 
 });
+
--- js/message.js
+++ js/message.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
 $('body').append("<div class='notice'><p>This portfolio is under contruction.<br> Please check  back again later</p></div>");
 setTimeout(function() {
   $('.notice').remove();
-}, 5000)
+}, 6000)
 
 });
