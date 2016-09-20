9e0cbea2053e62907a3e9459df6c388ab4a67c1c
Frantz Augustin <hirefrantz@gmail.com>
2016-09-19 20:58:58 -0400

    2.0

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $menu.on('click', function(){
    $('.nav').toggleClass('menu-open');
    $(".nav").css({"background-color": "black", "height":"100%", "z-index":"9999", "position":"absolute", "top":"0"});
-
+   $('.closed').removed('menu-open');
   });
 
 });
