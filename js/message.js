87fe1ae22d98c77c16465c23117cad2070f82390
Frantz Augustin <hirefrantz@gmail.com>
2016-09-20 10:11:36 -0400

    2.0

--- js/message.js
+++ js/message.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
 $('body').append(
   "<div class='notice'> \
+  <i class='fa fa-times'></i> \
   <p>This portfolio is under contruction.<br> Please check back later</p> \
   </div>"
   );
